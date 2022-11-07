8208e01d01dbf7a17a141c63399b8bdc9e5f2918
roukhmi <roukhmi02@gmail.com>
2022-11-08 00:31:28 +0100

    modification dans les pages de fxml

--- src/main/java/project/autocar_ticket/BusController.java
+++ src/main/java/project/autocar_ticket/BusController.java
@@ -1,4 +1,27 @@
 package project.autocar_ticket;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
 public class BusController {
+
+    private Button addbus;
+
+    @FXML
+    private void Addbus(MouseEvent event){
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Addbuspage.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 400, 400);
+            Stage stage=new Stage();
+            stage.setTitle("Add Bus");
+            stage.setScene(scene);
+            stage.show();
+        }catch (Exception e){
+
+        }
+    }
 }
