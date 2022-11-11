7e259423907dc48a54c89efd47ac116edead9ab3
Agourrame <newhamza2019@gmail.com>
2022-11-11 22:26:03 +0100

    add sql library

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module project.autocar_ticket {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
 
     opens project.autocar_ticket to javafx.fxml;
     exports project.autocar_ticket;
