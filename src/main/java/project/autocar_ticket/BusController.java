71a3026597c7737938803ddd66f9dc82149bfb15
roukhmi <roukhmi02@gmail.com>
2022-11-17 22:42:40 +0100

    add design and bus controller

--- src/main/java/project/autocar_ticket/BusController.java
+++ src/main/java/project/autocar_ticket/BusController.java
@@ -3,7 +3,8 @@ package project.autocar_ticket;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import java.sql.*;
@@ -25,4 +26,59 @@ public class BusController {
 
         }
     }
+
+    public TextField rn,startplace,endplace,price,nplace;
+    public ComboBox<String> chauffeur;
+    public DatePicker date;
+    public TableView<Autocar>tableautocar;
+
+    @FXML
+    public TableColumn<Autocar,Integer> colmunrn = new TableColumn<>();
+    public TableColumn<Autocar,String> colmunstartplace = new TableColumn<>();
+    public TableColumn<Autocar,String> colmunendplace = new TableColumn<>();
+    public TableColumn<Autocar,String> colmunchauffeur= new TableColumn<>();
+    public TableColumn<Autocar,String> colmundate = new TableColumn<>();
+    public TableColumn<Autocar,Integer> colmunprice= new TableColumn<>();
+    public TableColumn<Autocar,Integer> colmunnplace= new TableColumn<>();
+
+
+    //Database
+    String url = "jdbc:mysql://localhost/gestion_de_ticket";
+    String username="root";
+    String password="roukhmi1234";
+    Connection con;
+    PreparedStatement stmt;
+    ResultSet rs;
+
+
+    public void initialize (){
+        try {
+            con = DriverManager.getConnection(url,username,password);
+            System.out.println("Good");
+        }catch (Exception e){
+            System.out.println(e.toString());
+        }
+
+        chauffeur.getItems().addAll("Taroudant","Agadir","Marrakech","Casalanca","Tanja","Titouane");
+
+
+        colmunrn.setCellValueFactory(new PropertyValueFactory<>("rn"));
+        colmunstartplace.setCellValueFactory(new PropertyValueFactory<>("startplace"));
+        colmunendplace.setCellValueFactory(new PropertyValueFactory<>("endplace"));
+        colmundate.setCellValueFactory(new PropertyValueFactory<>("date"));
+        colmunprice.setCellValueFactory(new PropertyValueFactory<>("price"));
+        colmunnplace.setCellValueFactory(new PropertyValueFactory<>("nplace"));
+        colmunchauffeur.setCellValueFactory(new PropertyValueFactory<>("chauffeur"));
+    }
+
+    @FXML
+    public void addautocar(){
+        int autocarprice=Integer.parseInt(price.getText());
+        int autocarnplace=Integer.parseInt(nplace.getText());
+        Autocar car = new Autocar(rn.getText(),startplace.getText(),endplace.getText(),date.getValue().toString(),autocarprice,autocarnplace,chauffeur.getSelectionModel().getSelectedItem());
+        tableautocar.getItems().add(car);
+    }
+
+
 }
+
