5f8cc76de8a3d879bf4c0be7b4e5f65430acf54c
badrooxe <badrooxe55@gmail.com>
2022-11-18 19:56:18 +0100

    chauffeur details edited

--- src/main/java/project/autocar_ticket/ChauffeurController.java
+++ src/main/java/project/autocar_ticket/ChauffeurController.java
@@ -1,28 +1,30 @@
 package project.autocar_ticket;
 
+import Modules.Bus;
+import Modules.chauffeur;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import org.w3c.dom.events.MouseEvent;
 
 import java.net.URL;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class ChauffeurController implements Initializable {
-
 
     //database
     String url = "jdbc:mysql://localhost/gestion_de_teckit";
     String username="root";
     String password="";
     Connection con;
-    PreparedStatement ps;
+    PreparedStatement stmt;
     ResultSet rs;
 
     //--------------------------
@@ -31,34 +33,69 @@ public class ChauffeurController implements Initializable {
     @FXML
     private TextField cininput;
     //--------------------------
+    @FXML
+    private TableView<chauffeur> table;
+    @FXML
+    private TableColumn<chauffeur,String> tablecin= new TableColumn<>();
 
-    /*
     @FXML
-    private Button addch;
-    @FXML
-    private Button updatwch;
-    @FXML
-    private Button deletech;
-    */
+    private TableColumn<chauffeur,Integer> tableid= new TableColumn<>();
 
+    @FXML
+    private TableColumn<chauffeur,String> tablename= new TableColumn<>();
     //--------------------------
     @FXML
-    private TableColumn tableid;
-    @FXML
-    private TableColumn tablename;
-    @FXML
-    private TableColumn tablecin;
-    //--------------------------
-    @FXML
-    void addch(MouseEvent event){
-
+    void addChauffeur(ActionEvent event){
+        try {
+            stmt=con.prepareStatement("INSERT INTO `chauffeur` (`nom`, `cin`) VALUES ('"+nameinput.getText()+"','"+cininput.getText()+"');");
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
+    @FXML
+    void updateChauffeur(ActionEvent event) {
+        int id=table.getSelectionModel().getSelectedItem().getId();
+        try {
+            stmt=con.prepareStatement("UPDATE `chauffeur` SET nom='"+nameinput.getText()+"',cin='"+cininput.getText()+"' WHERE id='"+id+"'");
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @FXML
+    void deleteChauffeur(ActionEvent event) {
+        int id=table.getSelectionModel().getSelectedItem().getId();
+        try {
+            stmt=con.prepareStatement("Delete FROM `chauffeur` WHERE id='"+id+"'");
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    void uploadChauffeurInfo(){
+        try{
+            stmt=con.prepareStatement("SELECT * FROM `chauffeur`");
+            rs=stmt.executeQuery();
 
-
-
-
-
-
+            while (rs.next()){
+                chauffeur newchauffeur=new chauffeur(
+                        rs.getInt("id"),
+                        rs.getString("nom"),
+                        rs.getString("cin")
+                );
+                this.table.getItems().add(newchauffeur);
+            }
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+    void getSelectedItem(){
+        table.setOnMouseClicked(event ->{
+            nameinput.setText(table.getSelectionModel().getSelectedItem().getNom());
+            cininput.setText(table.getSelectionModel().getSelectedItem().getCin());
+        });
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -68,7 +105,11 @@ public class ChauffeurController implements Initializable {
         }catch (Exception e){
             System.out.println(e.toString());
         }
+        uploadChauffeurInfo();
+        getSelectedItem();
+
+        tableid.setCellValueFactory(new PropertyValueFactory<>("id"));
+        tablename.setCellValueFactory(new PropertyValueFactory<>("nom"));
+        tablecin.setCellValueFactory(new PropertyValueFactory<>("cin"));
     }
-
-
 }
--- src/main/java/project/autocar_ticket/HelloApplication.java
+++ src/main/java/project/autocar_ticket/HelloApplication.java
@@ -33,5 +33,7 @@ public class HelloApplication extends Application {
 
 
 
-        public static void main(String[] args) {launch();}
+    public static void main(String[] args) {
+        launch();
+    }
 }
