d8c7c0969ed637bdff7b46b78b1701ab293ea482
badrooxe <badrooxe55@gmail.com>
2022-11-19 01:04:09 +0100

    new repo

--- src/main/java/project/autocar_ticket/HistorypageController.java
+++ src/main/java/project/autocar_ticket/HistorypageController.java
@@ -1,13 +1,17 @@
 package project.autocar_ticket;
 import Modules.History;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -17,7 +21,12 @@ import java.text.SimpleDateFormat;
 import java.util.ResourceBundle;
 
 public class HistorypageController implements Initializable {
-
+    @FXML
+    void close(ActionEvent event) throws IOException {
+        Stage stage1;
+        stage1 = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage1.close();
+    }
     //Database----------------------------------------
 
     String url = "jdbc:mysql://localhost/gestion_de_teckit";
@@ -101,24 +110,5 @@ public class HistorypageController implements Initializable {
         startcol.setCellValueFactory(new PropertyValueFactory("start"));
         endcol.setCellValueFactory(new PropertyValueFactory("end"));
 
-    }
-
-<<<<<<< HEAD
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.Node;
-import javafx.stage.Stage;
-
-import java.io.IOException;
-
-public class HistorypageController {
-=======
->>>>>>> a4745a6753ed8f723ba3590ad6cb4b5d4a3cbdfb
-
-    @FXML
-    void close(ActionEvent event) throws IOException {
-        Stage stage1;
-        stage1 = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage1.close();
     }
 }
